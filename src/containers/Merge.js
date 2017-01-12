4685068601f1a4856a36a1a54e598b17c193c221
Noranda Brown <noranda@norandabrown.com>
2017-01-12 16:53:30 -0500

    WIP - Upgraded to Bootstrap 4, implemented header for merge tool and styled

--- src/containers/Merge.js
+++ src/containers/Merge.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesome';
 
 import PageHeader from '../components/Header/PageHeader';
 
@@ -7,6 +8,53 @@ export default class Merge extends Component {
     return (
       <div className="merge">
         <PageHeader title="Merge" />
+
+        <div className="merge-tool">
+          <div className="merge-tool-header">
+            <div className="merge-tool-header-titles d-flex justify-content-around">
+              <div className="merge-tool-header-title">Source</div>
+              <div className="merge-tool-header-title">Target</div>
+              <div className="merge-tool-header-title">Source</div>
+            </div>
+
+            <div className="merge-tool-header-selectors d-flex justify-content-around">
+              <div className="merge-tool-header-selector source d-flex">
+                <div className="merge-tool-header-selector-icon source-1">
+                  <FontAwesome name="user" />
+                  <span className="icon-line"></span>
+                </div>
+
+                <span className="source-name">89239857: Johnson, Edward</span>
+              </div>
+
+              <div className="merge-tool-header-selector target d-flex">
+                <div className="merge-tool-header-selector-icon target">
+                  <FontAwesome name="user" />
+                  <span className="icon-line"></span>
+                </div>
+
+                <span className="source-name">89239857: Johnson, Edward</span>
+              </div>
+
+              <div className="merge-tool-header-selector source d-flex">
+                <div className="merge-tool-header-selector-icon source-2">
+                  <FontAwesome name="user" />
+                  <span className="icon-line"></span>
+                </div>
+
+                <span className="source-name">98238592: Johnson, Eddy</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="merge-tool-body">
+
+          </div>
+
+          <div className="merge-tool-footer">
+
+          </div>
+        </div>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
   entry: [
     'bootstrap-loader',
     'font-awesome-sass-loader',
+    'tether',
     path.join(__dirname, "src", "index.js")
   ],
   output: {
@@ -22,7 +23,7 @@ module.exports = {
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff&name=assets/[name].[ext]' },
       { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader?name=assets/[name].[ext]' },
-      { test: /\.scss$/, loaders: ['style', 'css', 'sass'] }
+      { test: /\.scss$/, loaders: ['style', 'css', 'postcss', 'sass'] }
     ]
   },
 
@@ -37,11 +38,28 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src", "index.tmpl.html")
     }),
+    new webpack.HotModuleReplacementPlugin(),
     new webpack.ProvidePlugin({
-           $: "jquery",
-           jQuery: "jquery"
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      Tether: "tether",
+      "window.Tether": "tether",
+      Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
+      Button: "exports-loader?Button!bootstrap/js/dist/button",
+      Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
+      Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
+      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+      Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
+      Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
+      Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
+      Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
+      Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+      Util: "exports-loader?Util!bootstrap/js/dist/util",
     }),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.ProvidePlugin({
+      "window.Tether": "tether"
+    })
   ],
 
   devServer: {
